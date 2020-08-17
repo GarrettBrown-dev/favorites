d6d7c0c188cebd4d4687bfade27eae9a0a3ba431
donnymays <dtmays2@gmail.com>
2020-08-17 11:15:11 -0700

    working js to create favorites array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  $("formOne").submit(function(event) {
-    event.preventDefault();
+  $("#formOne").submit(function(event) {
     
-  
+
+    let food = $("input#food").val();
+    let animal = $("input#animal").val();
+    let place = $("input#place").val();
+    let movie = $("input#movie").val();
+    let seltzer = $("input#seltzer").val();
+
+    let favorites = [food, animal, place, movie, seltzer];
+   
+    $(".favorites").text(favorites);
+    
+    
+    event.preventDefault();
   });
 });
