5c60b6ddad70e6ac9476a09db82eef029417d716
donnymays <dtmays2@gmail.com>
2020-08-17 11:30:41 -0700

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,19 @@ $(document).ready(function() {
     let seltzer = $("input#seltzer").val();
 
     let favorites = [food, animal, place, movie, seltzer];
+    let someFavorites = [];
+    let favoriteTwo = favorites[1];
+    let favoriteOne = favorites[0];
+    let favoriteThree = favorites[2];
+
+    someFavorites.push(favoriteTwo);
+    someFavorites.push(favoriteOne);
+    someFavorites.push(favoriteThree);
+
+
+    $(".favoriteTwo").text(someFavorites[0]);
+    $(".favoriteOne").text(someFavorites[1]);
+    $(".favoriteThree").text(someFavorites[2]);
    
     $(".favorites").text(favorites);
     
